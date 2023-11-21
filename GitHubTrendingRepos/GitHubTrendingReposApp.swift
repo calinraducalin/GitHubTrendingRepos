4139816c8a496f0b3be6005bd9c006ee433643d5
calinraducalin <crcalin@gmail.com>
2023-11-21 21:02:55 +0200

    Initial Commit

---
+++ GitHubTrendingRepos/GitHubTrendingReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTrendingReposApp.swift
+//  GitHubTrendingRepos
+//
+//  Created by Calin Radu Calin on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTrendingReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubTrendingReposUITests/GitHubTrendingReposUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHubTrendingReposUITestsLaunchTests.swift
+//  GitHubTrendingReposUITests
+//
+//  Created by Calin Radu Calin on 21.11.2023.
+//
+
+import XCTest
+
+final class GitHubTrendingReposUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
